3803c7cd2a641fbf9c8e3ffa6b98b5b783a58b59
21r8390 <21r8390@bztf.ch>
2022-05-13 13:38:03 +0200

    XML to CSV

--- src/convert_csv.ps1
+++ src/convert_csv.ps1
@@ -1,11 +1,38 @@
-# Author: Joaquin koller
+# Author: Joaquin koller & Manuel Schumacher
 # Datum: 09.05.2022
-# Version: 1.0
+# Version: 1.5
 # Funktionsbeschreibung: Konvertiert CSV zu XML.
 # Parameter: keine
-# Bemerkungen:
+# Bemerkungen: XML hat bereits CSV Format
 #-----
 
-$path = "C:\Users\Koller\Documents\dev\m112-AD-Accounts\src\assets"
+# Set Assets Path
+$AssetsPath = ".\src\assets"
 
-Import-CSV "$($path)\schueler.csv" | Export-CliXML "$($path)\schueler.xml"
+# CSV in XML Konvertieren, da XML anscheinend kaputt
+Import-CSV -Path "$($AssetsPath)\schueler.csv" | Export-CliXML -Path "$($AssetsPath)\schueler.xml" 
+
+# XML auslesen
+[xml] $SchuelerXML = Get-Content -Path "$($AssetsPath)\schueler.xml"
+
+# Liste erstellen und bereits CSV-Headers hinzufügen
+[System.Collections.ArrayList] $CSV_Data = @($SchuelerXML.Objs.Obj.MS.S.N.GetValue(0))
+
+# CSV der Liste hinzufügen
+# Hinweis: XML hat bereits ein CSV Format
+$CSV_Data.AddRange($SchuelerXML.Objs.Obj.MS.S.InnerXML)
+
+# Testen ob CSV-Datei bereits existiert
+If (Test-Path "$($AssetsPath)\schueler.csv") {
+    # CSV löschen, da eine neue generiert wird
+    Remove-Item -Path "$($AssetsPath)\schueler.csv" -Force 
+}
+
+# Liste in die CSV-Datei schreiben
+foreach ($Linie in $CSV_Data) {
+    # Jede Linie der Datei hinzufügen
+    $Linie | Out-File -Encoding utf8 -FilePath "$($AssetsPath)\schueler.csv" -Append
+}
+
+# Status Meldung anzeigen
+Write-Host "CSV-Datei erfolgreich erstellt" -ForegroundColor Green
