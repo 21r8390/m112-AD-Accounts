f4bce572711b2368c421f8d7ada31293a16e61fe
21r9149 <kollerjoaquin13@gmail.com>
2022-05-22 22:44:15 +0200

    Feature: Disable-AD_accounts

---
+++ src/Disable-AD_accounts.ps1
@@ -0,0 +1,20 @@
+# Author: Joaquin koller & Manuel Schumacher
+# Datum: 22.05.2022
+# Version: 1.1
+# Funktionsbeschreibung: AD-Benutzer, welche nicht im XML vorhanden sind, deaktivieren
+# Parameter: keine
+# Bemerkungen: 
+#-----
+
+# Konfigurationen und Methoden laden
+. $PSScriptRoot\Config.ps1
+. $PSScriptRoot\ImportUsers.ps1
+
+function Disable-AD_accounts(){
+    # Methode aus "ImportUsers", Importiert alle Schüler als Liste aus dem CSV
+    $User_CSV = Get-SchulerFromCSV
+
+    # # Liste aller User - User aus der CSV Liste
+    #Get-AdUser -Filter * -Properties * | Where-Object { $User_CSV -notcontains $_.SamAccountName } | Select-Object SamAccountName, Name
+    Get-AdUser -Filter * -Properties * | Where-Object { $User_CSV -notcontains $_.SamAccountName } | Select-Object SamAccountName, Name
+}
