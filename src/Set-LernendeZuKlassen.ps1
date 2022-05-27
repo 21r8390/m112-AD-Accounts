778d28212871fdc657f365fb5ce949c318059985
21r8390 <21r8390@bztf.ch>
2022-05-27 15:59:24 +0200

    Alle Lernende zur Klasse hinzufügen fertig

--- src/Set-LernendeZuKlassen.ps1
+++ src/Set-LernendeZuKlassen.ps1
@@ -15,25 +15,44 @@ Function Set-LernendeZuKlassen {
         # Alle Lernende und Klassen aus CSV
         $Lernende = Get-Lernende
         Write-Log "Es wurden $($Lernende.Count) Lernende im CSV gefunden" -Level DEBUG
+
+        # Lernende aus AD lesen
+        $AdLernende = Get-AdUser -Filter '*'  -SearchBase "OU=$($Config.LERNENDE_OU),OU=$($Config.SCHULE_OU), $($Config.DOMAIN)"
+
+        # Klassen aus AD auslesen
+        $AdKlassen = Get-AdGroup -Filter '*'  -SearchBase "OU=$($Config.KLASSE_OU),OU=$($Config.SCHULE_OU), $($Config.DOMAIN)"
+        Write-Log "Es wurden $($AdKlassen.Count) Klassen im AD gefunden" -Level DEBUG
     }
     
     process {
-        # Zu Gruppe hinzufügen
-        $Lernende | ForEach-Object {
-            # $AdLernender = Get-AdUser 
 
-            # Klassen auslesen
-            $Klassen = Get-AdGroup -SearchBase "OU=$($Config.KLASSE_OU),OU=$($Config.SCHULE_OU),$($Config.DOMAIN)" -Filter "(Name -eq '$($Config.SCHULE_OU)_$($_.Klasse)') -or (Name -eq '$($Config.SCHULE_OU)_$($_.Klasse2)')"
-        
-            # Aktuelle Klassen auslesen
-            # $AktuelleKlassen = Get-ADPrincipalGroupMembership -Identity "CN=$($_.SamAccountName ),OU=$($Config.LERNENDE_OU),OU=$($Config.SCHULE_OU), $($Config.DOMAIN)"
+        $AdKlassen | ForEach-Object {
+            # Aktuelle Klasse zwischenspeichern, für verschachtelte Schleifen
+            $Klasse = $_
 
-            # Zu Klassen hinzufügen
-            $Klassen | ForEach-Object {
-                Add-ADGroupMember -Identity $_ -Member "CN=$($_.Name),OU=$($Config.KLASSE_OU),OU=$($Config.SCHULE_OU),$($Config.DOMAIN)"
-                Write-Log "Füge $($_.Name) zu $($_.SamAccountName) hinzu" -Level DEBUG
+            # Lernende, welche zur Klasse gehören auslesen
+            $Members = $AdLernende | Where-Object { 
+                ($Lernende | Where-Object { 
+                    # Alle Lernende (CSV) anhand der Klasse (AD) herausfiltern
+                    ($Klasse.Name -eq ($Config.SCHULE_OU + "_" + $_.Klasse)) -or ($Klasse.Name -eq ($Config.SCHULE_OU + "_" + $_.Klasse2))
+                    
+                    # Herausgefilterte Lernende (CSV) auf AdLernende (AD) mappen
+                } | Select-Object -Property SamAccountName).SamAccountName -Contains $_.SamAccountName
             }
+
+            # Lernende die nicht mehr in Klasse sein sollten herausfiltern
+            $NichtMehrInKlasse = Get-ADGroupMember -Identity $Klasse | Where-Object { ! ($Members.SamAccountName -Contains $_.SamAccountName) }
+            if ($NichtMehrInKlasse) {
+                # Lernende aus Klasse entfernen
+                Remove-ADGroupMember -Identity $Klasse -Members $NichtMehrInKlasse -Confirm:$false
+                Write-Log "Es wurden $($NichtMehrInKlasse.SamAccountName -join ', ') von der $($Klasse.Name) entfernt" -Level WARN
+            }
+
+            # Alle Lernende zur Klasse hinzufügen
+            Add-ADGroupMember -Identity $Klasse -Members $Members 
+
+            # Log Meldungen
+            Write-Log "Es wurden $($Members.SamAccountName -join ', ') zur Klasse $($Klasse.Name) hinzugefügt" -Level INFO
         }
     }
 }
-Set-LernendeZuKlassen
